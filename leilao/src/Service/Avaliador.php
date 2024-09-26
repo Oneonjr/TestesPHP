c25b7477f8eada1ecdde4adee3509f1eb2fbb589
Oneonjr <oneonjunior@outlook.com>
2024-09-26 13:00:10 -0300

    Melhoria Avaliador

--- leilao/src/Service/Avaliador.php
+++ leilao/src/Service/Avaliador.php
@@ -3,11 +3,13 @@
 namespace Alura\Leilao\Service;
 
 use Alura\Leilao\Model\Leilao;
+use Alura\Leilao\Model\Lance;
 
 class Avaliador
 {
     private $maiorValor = -INF;
     private $menorValor = INF;
+    private $maioresLances;//Melhoria avaliador
 
     public function avalia(Leilao $leilao): void
     {
@@ -20,6 +22,12 @@ class Avaliador
                 $this->menorValor = $lance->getValor();
             }
         }
+
+        $lances = $leilao->getLances();//Melhoria avaliador
+        usort($lances, function (Lance $lance1, Lance $lance2) {//Melhoria avaliador
+            return $lance2->getValor() - $lance1->getValor();
+        });
+        $this->maioresLances = array_slice($lances, 0, 3);
     }
 
     public function getMaiorValor(): float
@@ -30,5 +38,13 @@ class Avaliador
     public function getMenorValor(): float
     {
         return $this->menorValor;
+    }
+
+    /**
+    * @return Lance[]
+    */
+    public function getMaioresLances(): array//Melhoria avaliador
+    {
+    return $this->maioresLances;
     }
 }
